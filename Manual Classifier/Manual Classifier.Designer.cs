10e34d4d83cf65ff6fe49073e85b7fae6049c811
조아진 <ajcho97@khu.ac.kr>
2018-02-14 17:42:47 +0100

    prev.next

--- Manual Classifier/Manual Classifier.Designer.cs
+++ Manual Classifier/Manual Classifier.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Correct = new System.Windows.Forms.Button();
+            this.btn_Wrong = new System.Windows.Forms.Button();
+            this.btn_Other = new System.Windows.Forms.Button();
+            this.btn_Prev = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.pb_cam1 = new System.Windows.Forms.PictureBox();
+            this.pb_cam2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_cam1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_cam2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Correct
+            // 
+            this.btn_Correct.Location = new System.Drawing.Point(1266, 20);
+            this.btn_Correct.Name = "btn_Correct";
+            this.btn_Correct.Size = new System.Drawing.Size(100, 100);
+            this.btn_Correct.TabIndex = 0;
+            this.btn_Correct.Text = "Correct";
+            this.btn_Correct.UseVisualStyleBackColor = true;
+            this.btn_Correct.Click += new System.EventHandler(this.btn_Correct_Click);
+            // 
+            // btn_Wrong
+            // 
+            this.btn_Wrong.Location = new System.Drawing.Point(1266, 126);
+            this.btn_Wrong.Name = "btn_Wrong";
+            this.btn_Wrong.Size = new System.Drawing.Size(100, 100);
+            this.btn_Wrong.TabIndex = 1;
+            this.btn_Wrong.Text = "Wrong";
+            this.btn_Wrong.UseVisualStyleBackColor = true;
+            this.btn_Wrong.Click += new System.EventHandler(this.btn_Wrong_Click);
+            // 
+            // btn_Other
+            // 
+            this.btn_Other.Location = new System.Drawing.Point(1266, 232);
+            this.btn_Other.Name = "btn_Other";
+            this.btn_Other.Size = new System.Drawing.Size(100, 100);
+            this.btn_Other.TabIndex = 2;
+            this.btn_Other.Text = "Other";
+            this.btn_Other.UseVisualStyleBackColor = true;
+            this.btn_Other.Click += new System.EventHandler(this.btn_Other_Click);
+            // 
+            // btn_Prev
+            // 
+            this.btn_Prev.Location = new System.Drawing.Point(1266, 414);
+            this.btn_Prev.Name = "btn_Prev";
+            this.btn_Prev.Size = new System.Drawing.Size(100, 100);
+            this.btn_Prev.TabIndex = 3;
+            this.btn_Prev.Text = "Prev";
+            this.btn_Prev.UseVisualStyleBackColor = true;
+            this.btn_Prev.Click += new System.EventHandler(this.btn_Prev_Click);
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Location = new System.Drawing.Point(1266, 520);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(100, 100);
+            this.btn_Next.TabIndex = 4;
+            this.btn_Next.Text = "Next";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // pb_cam1
+            // 
+            this.pb_cam1.Location = new System.Drawing.Point(24, 20);
+            this.pb_cam1.Name = "pb_cam1";
+            this.pb_cam1.Size = new System.Drawing.Size(600, 600);
+            this.pb_cam1.TabIndex = 5;
+            this.pb_cam1.TabStop = false;
+            // 
+            // pb_cam2
+            // 
+            this.pb_cam2.Location = new System.Drawing.Point(642, 20);
+            this.pb_cam2.Name = "pb_cam2";
+            this.pb_cam2.Size = new System.Drawing.Size(600, 600);
+            this.pb_cam2.TabIndex = 6;
+            this.pb_cam2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1391, 639);
+            this.Controls.Add(this.pb_cam2);
+            this.Controls.Add(this.pb_cam1);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.btn_Prev);
+            this.Controls.Add(this.btn_Other);
+            this.Controls.Add(this.btn_Wrong);
+            this.Controls.Add(this.btn_Correct);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Manual Classifier";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_cam1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_cam2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Correct;
+        private System.Windows.Forms.Button btn_Wrong;
+        private System.Windows.Forms.Button btn_Other;
+        private System.Windows.Forms.Button btn_Prev;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.PictureBox pb_cam1;
+        private System.Windows.Forms.PictureBox pb_cam2;
     }
 }
 
--- Manual Classifier/Manual Classifier.cs
+++ Manual Classifier/Manual Classifier.cs
@@ -7,14 +7,170 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Manual_Classifier
 {
     public partial class Form1 : Form
     {
+        FileInfo[] images1 = null;
+        FileInfo[] images2 = null;
+
+        private int max;
+        private int idx;
+
         public Form1()
         {
             InitializeComponent();
+
+            DirectoryInfo dir1 = new DirectoryInfo(@"폴더경로");
+            DirectoryInfo dir2 = new DirectoryInfo(@"폴더경로");
+            
+            // 폴더 내 이미지 저장
+            FileInfo[] pic1 = dir1.GetFiles("*.jpg", SearchOption.AllDirectories);
+            FileInfo[] pic2 = dir2.GetFiles("*.jpg", SearchOption.AllDirectories);
+
+            images1 = new FileInfo[pic1.Length];
+            images2 = new FileInfo[pic2.Length];
+
+            pic1.CopyTo(images1, 0);
+            pic2.CopyTo(images2, 0);
+
+            // 변수 값 설정
+            max += pic1.Length;
+            idx = 0;
+
+            // 분류된 파일 존재
+            if(System.IO.File.Exists("파일경로"))
+            {
+
+            }
+            // 분류된 파일 없음
+            else
+            {
+                Bitmap nowImage1 = new Bitmap(images1[idx].FullName);
+                pb_cam1.Image = nowImage1;
+                Bitmap nowImage2 = new Bitmap(images2[idx].FullName);
+                pb_cam2.Image = nowImage2;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Correct (1)
+            if(e.KeyCode == Keys.NumPad1)
+            {
+                ////////////molar
+            }
+            // Wrong (2)
+            else if(e.KeyCode == Keys.NumPad2)
+            {
+
+            }
+            // Other (3)
+            else if(e.KeyCode == Keys.NumPad3)
+            {
+
+            }
+            // Prev (Left Arrow)
+            else if(e.KeyCode == Keys.Left)
+            {
+                idx--;
+                if (idx == -1)
+                {
+                    Message err = new Message();
+                    err.Text = "There is no previous image.";
+                    err.Show();
+                }
+                else
+                {
+                    Bitmap nowImage1 = new Bitmap(images1[idx].FullName);
+                    pb_cam1.Image = nowImage1;
+                    Bitmap nowImage2 = new Bitmap(images2[idx].FullName);
+                    pb_cam2.Image = nowImage2;
+                }
+            }
+            // Next (Right Arrow)
+            else if (e.KeyCode == Keys.Right)
+            {
+                idx++;
+                if (idx == max)
+                {
+                    Message err = new Message();
+                    err.Text = "There is no later image.";
+                    err.Show();
+                }
+                else
+                {
+                    Bitmap nowImage1 = new Bitmap(images1[idx].FullName);
+                    pb_cam1.Image = nowImage1;
+                    Bitmap nowImage2 = new Bitmap(images2[idx].FullName);
+                    pb_cam2.Image = nowImage2;
+                }
+            }
+            // Close (ESC)
+            else if(e.KeyCode == Keys.Escape)
+            {
+                // 파일 저장
+                Application.Exit();
+            }
+        }
+
+        private void btn_Correct_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Wrong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Other_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Prev_Click(object sender, EventArgs e)
+        {
+            idx--;
+            if (idx == -1)
+            {
+                Message err = new Message();
+                err.Text = "There is no previous image.";
+                err.Show();
+            }
+            else
+            {
+                Bitmap nowImage1 = new Bitmap(images1[idx].FullName);
+                pb_cam1.Image = nowImage1;
+                Bitmap nowImage2 = new Bitmap(images2[idx].FullName);
+                pb_cam2.Image = nowImage2;
+            }
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+            idx++;
+            if (idx == max)
+            {
+                Message err = new Message();
+                err.Text = "There is no later image.";
+                err.Show();
+            }
+            else
+            {
+                Bitmap nowImage1 = new Bitmap(images1[idx].FullName);
+                pb_cam1.Image = nowImage1;
+                Bitmap nowImage2 = new Bitmap(images2[idx].FullName);
+                pb_cam2.Image = nowImage2;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 파일 저장
+            e.Cancel = true;
         }
     }
 }

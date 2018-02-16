996b77d64a2d83618f944c68ced22854d42079c7
조아진 <ajcho97@khu.ac.kr>
2018-02-16 11:30:01 +0100

    half

--- Manual Classifier/Manual Classifier.Designer.cs
+++ Manual Classifier/Manual Classifier.Designer.cs
@@ -35,15 +35,20 @@
             this.btn_Next = new System.Windows.Forms.Button();
             this.pb_cam1 = new System.Windows.Forms.PictureBox();
             this.pb_cam2 = new System.Windows.Forms.PictureBox();
+            this.txt_curr = new System.Windows.Forms.TextBox();
+            this.txt_total = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_MoveTo = new System.Windows.Forms.TextBox();
+            this.btn_Jump = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pb_cam1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_cam2)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_Correct
             // 
-            this.btn_Correct.Location = new System.Drawing.Point(1266, 20);
+            this.btn_Correct.Location = new System.Drawing.Point(1050, 50);
             this.btn_Correct.Name = "btn_Correct";
-            this.btn_Correct.Size = new System.Drawing.Size(100, 100);
+            this.btn_Correct.Size = new System.Drawing.Size(80, 80);
             this.btn_Correct.TabIndex = 0;
             this.btn_Correct.Text = "Correct";
             this.btn_Correct.UseVisualStyleBackColor = true;
@@ -51,9 +56,9 @@
             // 
             // btn_Wrong
             // 
-            this.btn_Wrong.Location = new System.Drawing.Point(1266, 126);
+            this.btn_Wrong.Location = new System.Drawing.Point(1050, 136);
             this.btn_Wrong.Name = "btn_Wrong";
-            this.btn_Wrong.Size = new System.Drawing.Size(100, 100);
+            this.btn_Wrong.Size = new System.Drawing.Size(80, 80);
             this.btn_Wrong.TabIndex = 1;
             this.btn_Wrong.Text = "Wrong";
             this.btn_Wrong.UseVisualStyleBackColor = true;
@@ -61,9 +66,9 @@
             // 
             // btn_Other
             // 
-            this.btn_Other.Location = new System.Drawing.Point(1266, 232);
+            this.btn_Other.Location = new System.Drawing.Point(1050, 222);
             this.btn_Other.Name = "btn_Other";
-            this.btn_Other.Size = new System.Drawing.Size(100, 100);
+            this.btn_Other.Size = new System.Drawing.Size(80, 80);
             this.btn_Other.TabIndex = 2;
             this.btn_Other.Text = "Other";
             this.btn_Other.UseVisualStyleBackColor = true;
@@ -71,9 +76,9 @@
             // 
             // btn_Prev
             // 
-            this.btn_Prev.Location = new System.Drawing.Point(1266, 414);
+            this.btn_Prev.Location = new System.Drawing.Point(1050, 389);
             this.btn_Prev.Name = "btn_Prev";
-            this.btn_Prev.Size = new System.Drawing.Size(100, 100);
+            this.btn_Prev.Size = new System.Drawing.Size(80, 80);
             this.btn_Prev.TabIndex = 3;
             this.btn_Prev.Text = "Prev";
             this.btn_Prev.UseVisualStyleBackColor = true;
@@ -81,9 +86,9 @@
             // 
             // btn_Next
             // 
-            this.btn_Next.Location = new System.Drawing.Point(1266, 520);
+            this.btn_Next.Location = new System.Drawing.Point(1050, 475);
             this.btn_Next.Name = "btn_Next";
-            this.btn_Next.Size = new System.Drawing.Size(100, 100);
+            this.btn_Next.Size = new System.Drawing.Size(80, 80);
             this.btn_Next.TabIndex = 4;
             this.btn_Next.Text = "Next";
             this.btn_Next.UseVisualStyleBackColor = true;
@@ -91,25 +96,79 @@
             // 
             // pb_cam1
             // 
-            this.pb_cam1.Location = new System.Drawing.Point(24, 20);
+            this.pb_cam1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_cam1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_cam1.Location = new System.Drawing.Point(19, 53);
             this.pb_cam1.Name = "pb_cam1";
-            this.pb_cam1.Size = new System.Drawing.Size(600, 600);
+            this.pb_cam1.Size = new System.Drawing.Size(500, 500);
+            this.pb_cam1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pb_cam1.TabIndex = 5;
             this.pb_cam1.TabStop = false;
             // 
             // pb_cam2
             // 
-            this.pb_cam2.Location = new System.Drawing.Point(642, 20);
+            this.pb_cam2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_cam2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_cam2.Location = new System.Drawing.Point(535, 53);
             this.pb_cam2.Name = "pb_cam2";
-            this.pb_cam2.Size = new System.Drawing.Size(600, 600);
+            this.pb_cam2.Size = new System.Drawing.Size(500, 500);
+            this.pb_cam2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pb_cam2.TabIndex = 6;
             this.pb_cam2.TabStop = false;
+            // 
+            // txt_curr
+            // 
+            this.txt_curr.Location = new System.Drawing.Point(19, 14);
+            this.txt_curr.Name = "txt_curr";
+            this.txt_curr.Size = new System.Drawing.Size(100, 28);
+            this.txt_curr.TabIndex = 7;
+            this.txt_curr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_total
+            // 
+            this.txt_total.Location = new System.Drawing.Point(155, 14);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(100, 28);
+            this.txt_total.TabIndex = 8;
+            this.txt_total.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "of";
+            // 
+            // txt_MoveTo
+            // 
+            this.txt_MoveTo.Location = new System.Drawing.Point(849, 14);
+            this.txt_MoveTo.Name = "txt_MoveTo";
+            this.txt_MoveTo.Size = new System.Drawing.Size(100, 28);
+            this.txt_MoveTo.TabIndex = 10;
+            this.txt_MoveTo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_Jump
+            // 
+            this.btn_Jump.Location = new System.Drawing.Point(955, 14);
+            this.btn_Jump.Name = "btn_Jump";
+            this.btn_Jump.Size = new System.Drawing.Size(80, 30);
+            this.btn_Jump.TabIndex = 11;
+            this.btn_Jump.Text = "Jump";
+            this.btn_Jump.UseVisualStyleBackColor = true;
+            this.btn_Jump.Click += new System.EventHandler(this.btn_MoveTo_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1391, 639);
+            this.ClientSize = new System.Drawing.Size(1148, 574);
+            this.Controls.Add(this.btn_Jump);
+            this.Controls.Add(this.txt_MoveTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_total);
+            this.Controls.Add(this.txt_curr);
             this.Controls.Add(this.pb_cam2);
             this.Controls.Add(this.pb_cam1);
             this.Controls.Add(this.btn_Next);
@@ -119,12 +178,14 @@
             this.Controls.Add(this.btn_Correct);
             this.KeyPreview = true;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Manual Classifier";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pb_cam1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_cam2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -137,6 +198,11 @@
         private System.Windows.Forms.Button btn_Next;
         private System.Windows.Forms.PictureBox pb_cam1;
         private System.Windows.Forms.PictureBox pb_cam2;
+        private System.Windows.Forms.TextBox txt_curr;
+        private System.Windows.Forms.TextBox txt_total;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_MoveTo;
+        private System.Windows.Forms.Button btn_Jump;
     }
 }
 
--- Manual Classifier/Manual Classifier.cs
+++ Manual Classifier/Manual Classifier.cs
@@ -19,32 +19,76 @@ namespace Manual_Classifier
         private int max;
         private int idx;
 
-        Dictionary<string, int> result; // result dictionary 
+        Dictionary<string, int> result;         // result dictionary 
         string filePath = @"./classifier.out";  // file path to be saved
 
-        private string extractFileName(string fullName)
+        public Form1()
         {
-            int position = fullName.LastIndexOf('\\');
-            string fileName = fullName.Substring(position + 1);
-            return fileName;
+            InitializeComponent();
+
+            DirectoryInfo dir1 = new DirectoryInfo(@"./cam1");  // you sould change here based on your developing environment
+            DirectoryInfo dir2 = new DirectoryInfo(@"./cam2");  // you sould change here based on your developing environment
+
+            // save images in directory
+            images1 = dir1.GetFiles("*.jpg", SearchOption.AllDirectories);
+            images2 = dir2.GetFiles("*.jpg", SearchOption.AllDirectories);
+
+            max = images1.Length;
+            idx = -1;
+
+            txt_total.Text = max.ToString();
+
+            // if result file exists
+            if(System.IO.File.Exists(filePath))
+            {
+
+            }
+            // if there is no file
+            else
+            {
+                using (StreamWriter output = new StreamWriter(filePath))
+                {
+                    foreach(FileInfo file in images1)
+                    {
+                        output.WriteLine(file.Name + " 0");
+                    }
+                }
+            }
         }
 
+        // set a pair of images
         private void setImage()
         {
+            txt_curr.Text = (idx + 1).ToString();
             Bitmap nowImage1 = new Bitmap(images1[idx].FullName);
             pb_cam1.Image = nowImage1;
             Bitmap nowImage2 = new Bitmap(images2[idx].FullName);
             pb_cam2.Image = nowImage2;
         }
 
+        private void showPrevErr()
+        {
+            if (MessageBox.Show("There is no previous image.", "", MessageBoxButtons.OK) == DialogResult.No)
+            {
+                this.Close();
+            }
+        }
+
+        private void showNextErr()
+        {
+            if (MessageBox.Show("There is no next image.", "", MessageBoxButtons.OK) == DialogResult.No)
+            {
+                this.Close();
+            }
+        }
+
         private void movePrev()
         {
             idx--;
-            if (idx == -1)
+            if (idx == -1 || idx == -2) 
             {
-                Message err = new Message();
-                err.Text = "There is no previous image.";
-                err.Show();
+                showPrevErr();
+                idx++;
             }
             else
             {
@@ -57,9 +101,8 @@ namespace Manual_Classifier
             idx++;
             if (idx == max)
             {
-                Message err = new Message();
-                err.Text = "There is no next image.";
-                err.Show();
+                showNextErr();
+                idx--;
             }
             else
             {
@@ -67,94 +110,38 @@ namespace Manual_Classifier
             }
         }
 
-        public Form1()
-        {
-            InitializeComponent();
-
-            DirectoryInfo dir1 = new DirectoryInfo(@"./cam1");  // you sould change here based on your developing environment
-            DirectoryInfo dir2 = new DirectoryInfo(@"./cam2");  // you sould change here based on your developing environment
-
-            // save images in directory
-            FileInfo[] pic1 = dir1.GetFiles("*.jpg", SearchOption.AllDirectories);
-            FileInfo[] pic2 = dir2.GetFiles("*.jpg", SearchOption.AllDirectories);
-
-            images1 = new FileInfo[pic1.Length];
-            images2 = new FileInfo[pic2.Length];
-
-            pic1.CopyTo(images1, 0);
-            pic2.CopyTo(images2, 0);
-
-            max += pic1.Length;
-            idx = 0;
-
-            // file exist
-            if (System.IO.File.Exists(filePath))
-            {
-                /*
-                string[] lines = System.IO.File.ReadAllLines(filePath);
-                int position = lines[idx].LastIndexOf(' ');
-                string fileName = lines[idx].Substring(0, position);
-                while (extractFileName(images1[idx].FullName) == fileName)
-                {
-                    idx++;
-                    position = lines[idx].LastIndexOf(' ');
-                    fileName = lines[idx].Substring(0, position);
-                }
-                */
-                
-                for (int i = 0; i < max; i++)
-                {
-                    using (StreamReader sr = new StreamReader(filePath))
-                    {
-                        bool found = false;
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            int position = line.LastIndexOf(' ');
-                            string fileName = line.Substring(0, position);
-                            if (fileName == extractFileName(images1[i].FullName))
-                            {
-                                found = true;
-                                break;
-                            }
-                        }
-                        if(!found)
-                        {
-                            idx = i;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            setImage();
-
-            result = new Dictionary<string, int>();
-        }
-
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (idx != -1 && idx != max)
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_MoveTo_Click(sender, e);
+            }
+            else if (idx == -1 && e.KeyCode == Keys.Left)
+            {
+                showPrevErr();
+            }
+            else if (idx == max && e.KeyCode == Keys.Right)
+            {
+                showNextErr();
+            }
+            else if (idx != -1 && idx != max)
             {
                 // Correct (1)
                 if (e.KeyCode == Keys.NumPad1)
                 {
-                    result[extractFileName(images1[idx].FullName)] = 1;
-                    //result.Add(images1[idx].FullName, 1);
+                    result[images1[idx].Name] = 1;
                     moveNext();
                 }
                 // Wrong (2)
                 else if (e.KeyCode == Keys.NumPad2)
                 {
-                    result[extractFileName(images1[idx].FullName)] = 2;
-                    //result.Add(images1[idx].FullName, 2);
+                    result[images1[idx].Name] = 2;
                     moveNext();
                 }
                 // Other (3)
                 else if (e.KeyCode == Keys.NumPad3)
                 {
-                    result[extractFileName(images1[idx].FullName)] = 3;
-                    //result.Add(images1[idx].FullName, 3);
+                    result[images1[idx].Name] = 3;
                     moveNext();
                 }
             }
@@ -171,40 +158,82 @@ namespace Manual_Classifier
             // Close (ESC)
             else if (e.KeyCode == Keys.Escape)
             {
-                // save the file before quitting
-
-
-                Application.Exit();
+                /* save file */
+                if (MessageBox.Show("Do you want to exit?", "", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    Application.Exit();
+                }
             }
         }
 
         private void btn_Correct_Click(object sender, EventArgs e)
         {
-            if (idx != -1 && idx != max)
+            if(idx == -1)
             {
-                result[extractFileName(images1[idx].FullName)] = 1;
-                //result.Add(images1[idx].FullName, 1);
+                showPrevErr();
+            }
+            else if(idx == max)
+            {
+                showNextErr();
+            }
+            else if (idx != -1 && idx != max)
+            {
+                result[images1[idx].Name] = 1;
                 moveNext();
             }
         }
 
         private void btn_Wrong_Click(object sender, EventArgs e)
         {
-            if (idx != -1 && idx != max)
+            if (idx == -1)
             {
-                result[extractFileName(images1[idx].FullName)] = 2;
-                //result.Add(images1[idx].FullName, 2);
+                showPrevErr();
+            }
+            else if (idx == max)
+            {
+                showNextErr();
+            }
+            else if (idx != -1 && idx != max)
+            {
+                result[images1[idx].Name] = 2;
                 moveNext();
             }
         }
 
         private void btn_Other_Click(object sender, EventArgs e)
         {
-            if (idx != -1 && idx != max)
+            if (idx == -1)
             {
-                result[extractFileName(images1[idx].FullName)] = 3;
-                //result.Add(images1[idx].FullName, 3);
+                showPrevErr();
+            }
+            else if (idx == max)
+            {
+                showNextErr();
+            }
+            else if (idx != -1 && idx != max)
+            {
+                result[images1[idx].Name] = 3;
                 moveNext();
+            }
+        }
+
+        private void btn_MoveTo_Click(object sender, EventArgs e)
+        {
+            int i;
+            for(i = 0; i < max; i++)
+            {
+                if (images1[i].Name.Replace(".jpg", "") == txt_MoveTo.Text.ToString())
+                {
+                    idx = i;
+                    setImage();
+                    txt_MoveTo.Text = null;
+                    return;
+                }
+            }
+
+            if (MessageBox.Show("There is no such image.", "", MessageBoxButtons.OK) == DialogResult.No)
+            {
+                this.Close();
             }
         }
 
@@ -220,44 +249,11 @@ namespace Manual_Classifier
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            // save the file before quitting
-            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            /* save file */
+            if(MessageBox.Show("Do you want to exit?", "", MessageBoxButtons.YesNo) == DialogResult.No)
             {
-                using (TextWriter tw = new StreamWriter(fs))
-                {
-                    if (System.IO.File.Exists(filePath))
-                    {
-                        foreach (KeyValuePair<string, int> kvp in result)
-                        {
-                            using (StreamReader sr = new StreamReader(filePath))
-                            {
-                                string buffer = sr.Read().ToString();
-
-                                while (!sr.EndOfStream)
-                                {
-                                    if (buffer == kvp.Key)
-                                    {
-                                        fs.Seek(kvp.Key.Length, SeekOrigin.Current);
-                                        tw.Write(kvp.Value);
-                                        break;
-                                    }
-                                    fs.Seek(kvp.Key.Length + kvp.Value.ToString().Length, SeekOrigin.Current);
-                                }
-                                tw.WriteLine(string.Format("{0} {1}", kvp.Key, kvp.Value));
-                            }
-                        }
-                    }
-                    else
-                    {
-                        foreach (KeyValuePair<string, int> kvp in result)
-                        {
-                            tw.WriteLine(string.Format("{0} {1}", kvp.Key, kvp.Value));
-                        }
-                    }
-                    //
-                    e.Cancel = true;
-                }
+                e.Cancel = true;
             }
         }
-    }
+    };
 };
